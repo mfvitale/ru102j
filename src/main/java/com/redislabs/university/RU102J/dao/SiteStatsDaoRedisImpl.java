0acc1e4c838fcc8a0b2ff674b2accd815779b141
mfvitale <mvitale86@gmail.com>
2021-01-30 18:25:16 +0100

    Challenge #2.2

--- src/main/java/com/redislabs/university/RU102J/dao/SiteStatsDaoRedisImpl.java
+++ src/main/java/com/redislabs/university/RU102J/dao/SiteStatsDaoRedisImpl.java
@@ -47,7 +47,7 @@ public class SiteStatsDaoRedisImpl implements SiteStatsDao {
             ZonedDateTime day = reading.getDateTime();
             String key = RedisSchema.getSiteStatsKey(siteId, day);
 
-            updateBasic(jedis, key, reading);
+            updateOptimized(jedis, key, reading);
         }
     }
 
@@ -81,6 +81,11 @@ public class SiteStatsDaoRedisImpl implements SiteStatsDao {
     // Challenge #3
     private void updateOptimized(Jedis jedis, String key, MeterReading reading) {
         // START Challenge #3
+        Transaction transaction = jedis.multi();
+            compareAndUpdateScript.updateIfGreater(transaction, key, SiteStats.maxWhField, reading.getWhGenerated());
+            compareAndUpdateScript.updateIfLess(transaction, key, SiteStats.minWhField, reading.getWhGenerated());
+            compareAndUpdateScript.updateIfGreater(transaction, key, SiteStats.maxCapacityField, getCurrentCapacity(reading));
+        transaction.exec();
         // END Challenge #3
     }
 
